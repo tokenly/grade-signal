28b9644a979648a5dc5959c2ba08c6d270fc21e3
Devon Weller <devon@tokenly.com>
2018-04-09 09:05:51 -0500

    Add tokenmap freshness checks

--- App/ExternalChecks.php
+++ App/ExternalChecks.php
@@ -7,6 +7,8 @@ use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\RequestException;
 use Tokenly\QuotebotClient\Client as QuotebotClient;
 use Tokenly\QuotebotClient\Mock\MemoryCacheStore;
+use Tokenly\TokenmapClient\Mock\MemoryCacheStore as TokenmapMemoryCacheStore;
+use Tokenly\TokenmapClient\TokenmapClient;
 
 /**
  * ExternalChecks
@@ -38,6 +40,12 @@ class ExternalChecks
                     'id' => 'quotebot_freshness',
                     'method' => 'quotebot_freshness',
                     'name' => 'Quotebot Freshness',
+                    'params' => [],
+                ],
+                [
+                    'id' => 'tokenmap_freshness',
+                    'method' => 'tokenmap_freshness',
+                    'name' => 'Tokenmap Freshness',
                     'params' => [],
                 ],
             ];
@@ -75,7 +83,31 @@ class ExternalChecks
             $quote_timestamp = isset($loaded_quote['time']) ? strtotime($loaded_quote['time']) : 0;
             $seconds_old = time() - $quote_timestamp;
             if ($seconds_old >= $stale_seconds) {
-                $notes[] = "Quote from provider {$loaded_quote['source']} for pair {$loaded_quote['pair']} was $seconds_old old.";
+                $notes[] = "Quote from provider {$loaded_quote['source']} for pair {$loaded_quote['pair']} was $seconds_old seconds old.";
+                $success = false;
+            }
+        }
+
+        if ($success) {
+            return ['up', null];
+        }
+
+        return ['down', implode("\n", $notes)];
+    }
+
+    public function runCheck_tokenmap_freshness($parameters)
+    {
+        $stale_seconds = env('TOKENMAP_FRESHNESS_TTL', 1800); // 30 minutes default
+
+        $success = true;
+        $notes = [];
+        $tokenmap_client = new TokenmapClient(env('TOKENMAP_CONNECTION_URL'), new TokenmapMemoryCacheStore());
+        $loaded_quote_data = $tokenmap_client->loadAllQuotesData();
+        foreach ($loaded_quote_data['quotes'] as $loaded_quote) {
+            $quote_timestamp = isset($loaded_quote['time']) ? strtotime($loaded_quote['time']) : 0;
+            $seconds_old = time() - $quote_timestamp;
+            if ($seconds_old >= $stale_seconds) {
+                $notes[] = "Tokenmap Quote from provider {$loaded_quote['source']} for pair {$loaded_quote['pair']} was $seconds_old seconds old.";
                 $success = false;
             }
         }
@@ -96,19 +128,19 @@ class ExternalChecks
             'msg_rates_age' => $parameters['age'],
             'msg_rates_incr' => $parameters['age'] / 4,
         ];
-        $result = $this->callRabbitmqAPI('queues/'.urlencode($parameters['vhost']).'/'.$parameters['queue'], $query);
+        $result = $this->callRabbitmqAPI('queues/' . urlencode($parameters['vhost']) . '/' . $parameters['queue'], $query);
         // echo "\$result: ".json_encode($result, 192)."\n";
 
         // get_no_ack_details
-        $stats_to_check = ['ack','get_no_ack'];
-        foreach($stats_to_check as $stat_name) {
+        $stats_to_check = ['ack', 'get_no_ack'];
+        foreach ($stats_to_check as $stat_name) {
             $checks = $parameters[$stat_name];
-            $rate = $result['message_stats'][$stat_name.'_details']['avg_rate'];
-            if (isset($checks['min']) AND $rate < $checks['min']) {
+            $rate = $result['message_stats'][$stat_name . '_details']['avg_rate'];
+            if (isset($checks['min']) and $rate < $checks['min']) {
                 $notes[] = "$stat_name was $rate. Needs to be {$checks['min']}";
                 $status = 'down';
             }
-            if (isset($checks['max']) AND $rate > $checks['max']) {
+            if (isset($checks['max']) and $rate > $checks['max']) {
                 $notes[] = "$stat_name was $rate.  Must be less than {$checks['max']}";
                 $status = 'down';
             }
@@ -117,23 +149,24 @@ class ExternalChecks
         if (isset($parameters['messages'])) {
             $msg_checks = $parameters['messages'];
             $msg_count = $result['messages'];
-            if (isset($msg_checks['min']) AND $msg_count < $msg_checks['min']) {
+            if (isset($msg_checks['min']) and $msg_count < $msg_checks['min']) {
                 $notes[] = "messages was $msg_count. Needs to be {$msg_checks['min']}";
                 $status = 'down';
             }
-            if (isset($msg_checks['max']) AND $msg_count > $msg_checks['max']) {
+            if (isset($msg_checks['max']) and $msg_count > $msg_checks['max']) {
                 $notes[] = "messages was $msg_count.  Must be less than {$msg_checks['max']}";
                 $status = 'down';
             }
         }
-        
+
         return [$status, implode("\n", $notes)];
     }
 
     // http://rabbitmq.lb.stagesvc.tokenly.co/api/queues/chainscout/bitcoinTestnet_tx?lengths_age=600&lengths_incr=5&msg_rates_age=600&msg_rates_incr=5&data_rates_age=600&data_rates_incr=5
 
-    protected function callRabbitmqAPI($path, $query=[]) {
-        $api_path = '/api/'.$path;
+    protected function callRabbitmqAPI($path, $query = [])
+    {
+        $api_path = '/api/' . $path;
 
         $client = new GuzzleClient();
 
@@ -141,7 +174,7 @@ class ExternalChecks
             'auth' => [env('RABBITMQ_LOGIN'), env('RABBITMQ_PASSWORD')],
         ];
 
-        $request = new \GuzzleHttp\Psr7\Request('GET', env('RABBITMQ_API_URL').$api_path);
+        $request = new \GuzzleHttp\Psr7\Request('GET', env('RABBITMQ_API_URL') . $api_path);
         $request = \GuzzleHttp\Psr7\modify_request($request, ['query' => http_build_query($query, null, '&', PHP_QUERY_RFC3986)]);
 
         // send request
@@ -176,7 +209,7 @@ class ExternalChecks
         }
 
         $json = json_decode($response->getBody(), true);
-        if (!is_array($json)) { throw new Exception("Unexpected response", 1); }
+        if (!is_array($json)) {throw new Exception("Unexpected response", 1);}
 
         return $json;
     }
