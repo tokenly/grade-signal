e14b1b21eea96274ee18ea88b64e8fa3884518fb
Devon Weller <devon@tokenly.com>
2018-07-18 10:37:48 -0500

    minimalist status page

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Environment;
+use App\Log;
+
+require __DIR__.'/../vendor/autoload.php';
+Environment::init(__DIR__.'/..');
+
+require(__DIR__.'/include/header.php');
+
+$self = $_SERVER['PHP_SELF'];
+echo <<<EOT
+<h1 class="mt-4">Grade Signal</h1>
+
+<div class="mt-4">
+<a class="btn btn-primary" href="/status.php">Status</a>
+</div>
+EOT;
+
+
+require(__DIR__.'/include/footer.php');
---
+++ public/status.php
@@ -0,0 +1,76 @@
+<?php
+
+use App\Environment;
+use App\Log;
+use App\State;
+use App\Store;
+
+require __DIR__.'/../vendor/autoload.php';
+Environment::init(__DIR__.'/..');
+date_default_timezone_set('America/Chicago');
+
+require(__DIR__.'/include/header.php');
+
+$show_down_only = $_GET['down'] ?? false;
+
+$self = $_SERVER['PHP_SELF'];
+echo <<<EOT
+
+<div class="float-right">
+<a class="btn btn-danger" href="{$self}?down=1">Show Down Only</a>
+<a class="btn btn-light" href="{$self}">Show All</a>
+</div>
+
+<h1 class="mt-4">Status</h1>
+
+<table class="table mt-3">
+<thead>
+<tr>
+    <th>Name</th>
+    <th>ID</th>
+    <th>Status</th>
+    <th>Time</th>
+</tr>
+</thead>
+<tbody>
+EOT;
+
+echo '<pre>';
+
+$store = Store::instance();
+foreach ($store->findAllStateIDs() as $state_id) {
+    $state = State::findByID($state_id);
+    if (!$state) { continue; }
+
+    $status = $state->status;
+
+    if ($show_down_only and $status == 'up') {
+        continue;
+    }
+
+    $name = $state->name;
+    $check_id = $state->check_id;
+    $status_class = $state->status == 'up' ? 'text-success' : 'text-danger';
+    $timestamp = $state->timestamp;
+
+    $date = date("Y-m-d h:i:s A T", $timestamp);
+    echo <<<EOT
+    <tr>
+    <td>{$name}</td>
+    <td>{$check_id}</td>
+    <td><span class="{$status_class}">{$status}</span></td>
+    <td>{$date}</td>
+    </tr>
+EOT;
+
+
+}
+
+echo <<<EOT
+</tbody>
+</table>
+
+EOT;
+
+
+require(__DIR__.'/include/footer.php');
